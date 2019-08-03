fb0723949f655727aff11dc964e7a5c4a6076b06
Maxim Kitsenko <maxim.kitsenko.r31@gmail.com>
2019-08-03 18:04:39 +0300

    tests code cleaned

---
+++ Chat/Chat.Dal.IntegrationTests/Dto/Room.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Chat.Dal.Dto;
+
+namespace Chat.Dal.IntegrationTests.Dto
+{
+    public static class TestObjectBuilder
+    {
+        public static Chat.Dal.Dto.Room CreateRoom()
+        {
+            var res = new Room()
+            {
+                CreatedTime = new DateTime(2019, 8, 3),
+                Name = "asp.net",
+                Softdelete = false,
+                
+            };
+            return res;
+        }
+    }
+}
--- Chat/Chat.Dal.IntegrationTests/Repository/Mssql/RoomRepository.cs
+++ Chat/Chat.Dal.IntegrationTests/Repository/Mssql/RoomRepository.cs
@@ -2,58 +2,28 @@
 using System.Reflection.Metadata.Ecma335;
 using Chat.Dal.Dto;
 using Chat.Dal.Repository;
-using Chat.Dal.Repository.mssql;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using Chat.Dal.IntegrationTests.Dto;
 using Xunit;
 
 namespace Chat.Dal.IntegrationTests.Repository.Mssql
 {
-    public class RoomRepositoryTest
+    public class RoomRepositoryTest : RepositoryTestBase
     {
-        public class TestContext
-        {
-            private readonly IRoomRepository _roomRepository;
-
-            public TestContext(IRoomRepository roomRepository)
-            {
-                _roomRepository = roomRepository;
-            }
-        }
-
-        private TestContext _testContext;
         private readonly IRoomRepository _roomRepository;
-        private readonly ServiceProvider serviceProvider;
 
 
         public RoomRepositoryTest()
         {
-            var collection = new ServiceCollection()
-                //Entity Framework contexts cannot be shared by two threads
-                //Entity Framework contexts are recommended to be Scoped
-                .AddDbContext<ChattingContext>()
-                .AddSingleton<IRoomRepository, RoomRepository>()
-                //.AddSingleton<IRoomRepository, RoomRepository>(ctx => new RoomRepository()
-                //    {Context = (ChattingContext) ctx.GetService(typeof(DbContext))})
-                .AddSingleton< TestContext>()
-                .AddSingleton<IMessageRepository, MessageRepository>();
-
-
-            serviceProvider = collection.BuildServiceProvider();
-            _roomRepository = serviceProvider.GetService<IRoomRepository>();
+            _roomRepository = ServiceProvider.GetService<IRoomRepository>();
         }
-
-        //public RoomRepositoryTest()
-        //{
-        //    _roomRepository = roomRepository; 
-        //    //_roomRepository = serviceProvider.get <IRoomRepository>();
-        //}
 
         [Fact]
         public void CreateRooms()
         {
             //a
-            var newRoom = new Room();
+            var newRoom = TestObjectBuilder.CreateRoom();
             //a
             var createdRooms = _roomRepository.CreateRooms(newRoom);
             //a
