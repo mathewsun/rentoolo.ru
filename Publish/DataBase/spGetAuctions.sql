0a83d8d183abe9a3c228a4693cc20262a7024ac7
Mathew Dubrovin <mathew.dubrovin@gmail.com>
2019-09-16 01:23:45 +0300

    publish updates

---
+++ Publish/DataBase/spGetAuctions.sql
@@ -0,0 +1,22 @@
+﻿ALTER PROCEDURE  [dbo].[spGetUserAuctions]
+@UserId uniqueidentifier
+AS
+BEGIN
+	SELECT 
+	   a.[Id]
+      ,a.[Name] AuctionOwnerName 
+      ,a.[StartPrice] 
+      ,a.[Created] CreationDate
+	  ,u.[UserName] 
+	FROM [Auctions] as a
+	LEFT JOIN [Users] as u
+	on a.[UserId] = u.[UserId]
+	where a.[UserId] = @userId
+	ORDER BY a.Id DESC
+END
+GO
+
+GRANT EXECUTE
+    ON OBJECT::[dbo].[spGetUserAuctions] TO PUBLIC
+    AS [dbo];
+	
