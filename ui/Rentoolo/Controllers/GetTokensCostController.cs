9755c393fabd8e8e1e1d1fb27eb65c9853452b83
Antares488 <antares5988@gmail.com>
2020-05-03 18:15:25 +0300

    add GetTokensCostController.cs

---
+++ ui/Rentoolo/Controllers/GetTokensCostController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Rentoolo.Model;
+
+namespace Rentoolo.Controllers
+{
+    public class GetTokensCostController : ApiController
+    {
+        public IHttpActionResult GetTokensCost()
+        {
+            var result = TokensDataHelper.GetTokensCosts();
+
+            return Ok(result);
+        }
+        
+    }
+}
