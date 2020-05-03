5761b6c35abcc6d99cc096798acb7a67141f0ca7
matthew <mathew.dubrovin@gmail.com>
2020-05-03 23:44:05 +0300

    updates

--- ui/Rentoolo/Model/QiwiPayment.cs
+++ ui/Rentoolo/Model/QiwiPayment.cs
@@ -2,14 +2,6 @@
 {
     public class QiwiPayment
     {
-        public string Login { get; set; }
-
-        public string Pwd { get; set; }
-
-        public double Amount { get; set; }
-
-        public string Recipient { get; set; }
-
-        public string Comment { get; set; }
+        
     }
 }
