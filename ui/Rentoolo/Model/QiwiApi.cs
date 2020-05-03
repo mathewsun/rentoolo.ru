b8fd9fd68fa1a9ee73e5155bb3fc7d97df514fb4
matthew <mathew.dubrovin@gmail.com>
2020-05-03 23:40:48 +0300

    updates

--- ui/Rentoolo/Model/QiwiApi.cs
+++ ui/Rentoolo/Model/QiwiApi.cs
@@ -12,32 +12,189 @@ namespace QiwiAggregator.Model
 {
     public class QiwiObject
     {
+        CookieDictionary MyCokies;
+
+        string UA = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36 OPR/43.0.2442.991";
+
+        HttpRequest def = new HttpRequest();
 
         public QiwiObject()
         {
+            MyCokies = new CookieDictionary();
+            def.Cookies = MyCokies;
+            def.AllowAutoRedirect = !true;
+            def.UserAgent = UA;
 
+            def.IgnoreProtocolErrors = true;
         }
 
         public bool auth(string login, string pass)
         {
             bool check = false;
+            using (var request = def)
+            {
+                HttpResponse resp = null;
+                string tresp = string.Empty;
+                request.Get("https://qiwi.com/");
 
+                var pdata = ("{'login':'+" + login + "','password':'" + pass + "','captcha':''}").Replace('\'', '"');
+                request.Referer = "https://qiwi.com/";
+                request.AddHeader("Accept", "application/vnd.qiwi.sso-v1+json");
+
+                resp = request.Post("https://sso.qiwi.com/cas/tgts", pdata, "application/json");
+                tresp = resp.ToString();
+
+                if (tresp.Contains(@"error"":{""message"))
+                    check = false;
+                else if (tresp.Contains(@"""ticket"":""TGT"))
+                {
+                    pdata = Regex.Match(tresp, @"ticket.{3}([^""]+)").Groups[1].Value;
+
+                    pdata = ("{'ticket':'" + pdata + "','service':'https://qiwi.com/j_spring_cas_security_check'}").Replace('\'', '"');
+                    request.Referer = "https://sso.qiwi.com/app/proxy?v=1";
+                    resp = request.Post("https://sso.qiwi.com/cas/sts", pdata, "application/json");
+                    tresp = resp.ToString();
+                    pdata = Regex.Match(tresp, @"ticket.{3}([^""]+)").Groups[1].Value;
+                    resp = request.Get("https://qiwi.com/j_spring_cas_security_check?ticket=" + pdata);
+
+                    check = true;
+                }
+
+                resp = request.Get("https://qiwi.com/cards.action");
+
+                tresp = resp.ToString();
+
+                var tresp1 = tresp.IndexOf("<div class=\"account_current_amount\">");
+
+                var tresp2 = tresp.Substring(tresp1 + 40);
+
+                string endofstring = "\n";
+
+                var indexTresp2 = tresp2.IndexOf(endofstring);
+
+                var stringBalance = tresp2.Substring(0, indexTresp2);
+
+                double balance = 0;
+
+                if (Double.TryParse(stringBalance, out balance))
+                {
+                    QiwiHelper.UpdateQiwiAccountBalance(login, balance);
+                }
+            }
             return check;
         }
 
         public void CheckQiwiAccountBalance(string login, string pass)
         {
+            bool check = false;
+            using (var request = def)
+            {
+                HttpResponse resp = null;
+                string tresp = string.Empty;
+                request.Get("https://qiwi.com/");
 
+                var pdata = ("{'login':'+" + login + "','password':'" + pass + "','captcha':''}").Replace('\'', '"');
+                request.Referer = "https://qiwi.com/";
+                request.AddHeader("Accept", "application/vnd.qiwi.sso-v1+json");
+
+                resp = request.Post("https://sso.qiwi.com/cas/tgts", pdata, "application/json");
+                tresp = resp.ToString();
+
+                if (tresp.Contains(@"error"":{""message"))
+                    check = false;
+                else if (tresp.Contains(@"""ticket"":""TGT"))
+                {
+                    pdata = Regex.Match(tresp, @"ticket.{3}([^""]+)").Groups[1].Value;
+
+                    pdata = ("{'ticket':'" + pdata + "','service':'https://qiwi.com/j_spring_cas_security_check'}").Replace('\'', '"');
+                    request.Referer = "https://sso.qiwi.com/app/proxy?v=1";
+                    resp = request.Post("https://sso.qiwi.com/cas/sts", pdata, "application/json");
+                    tresp = resp.ToString();
+                    pdata = Regex.Match(tresp, @"ticket.{3}([^""]+)").Groups[1].Value;
+                    resp = request.Get("https://qiwi.com/j_spring_cas_security_check?ticket=" + pdata);
+
+                    check = true;
+                }
+
+                resp = request.Get("https://qiwi.com/settings.action");
+
+                tresp = resp.ToString();
+
+                var tresp1 = tresp.IndexOf("<div class=\"account_current_amount\">");
+
+                var tresp2 = tresp.Substring(tresp1 + 40);
+
+                string endofstring = "\n";
+
+                var indexTresp2 = tresp2.IndexOf(endofstring);
+
+                var stringBalance = tresp2.Substring(0, indexTresp2);
+
+                double balance = 0;
+
+                if (Double.TryParse(stringBalance, out balance))
+                {
+                    QiwiHelper.UpdateQiwiAccountBalance(login, balance);
+                }
+            }
         }
 
         public bool payments(string account, double amount, string comment = "")
         {
-            return false;
+            var data = ("{'id':'" + DateTimeToUnixTimestamp(DateTime.Now) +
+                "','sum':{'amount':" + amount.ToString().Replace(',', '.') +
+                ",'currency':'643'},'source':'account_643','paymentMethod':{'type':'Account','accountId':'643'},'comment':'" +
+                comment
+                + "','fields':{'account':'" + account + "','_meta_pay_partner':'','browser_user_agent_crc':'d7dc4c39'}}").Replace('\'', '"');
+
+            using (var req = def)
+            {
+                req.Referer = "https://qiwi.com/payment/form.action?provider=99&state=confirm";
+
+                req.AddHeader("Accept", "application/vnd.qiwi.v2+json");
+                var resp = req.Post("https://qiwi.com/user/sinap/api/terms/99/payments/proxy.action", data, "application/json").ToString();
+
+                dynamic jsondata = JsonConvert.DeserializeObject<dynamic>(resp);
+
+                try
+                {
+                    if (jsondata.data.body.transaction.state.code == "Accepted")
+                    {
+                        return true;
+                    }
+                }
+                catch { }
+
+                return false;
+            }
         }
 
         public bool paymentBankCard(string account, double amount)
         {
-            return false;
+            var data = ("{'id':'" + DateTimeToUnixTimestamp(DateTime.Now) +
+                "','sum':{'amount':" + amount.ToString().Replace(',', '.') +
+                ",'currency':'643'},'source':'account_643','paymentMethod':{'type':'Account','accountId':'643'},'comment':'','fields':{'account':'" + account + "','_meta_pay_partner':'','browser_user_agent_crc':'d7dc4c39'}}").Replace('\'', '"');
+
+            using (var req = def)
+            {
+                req.Referer = "https://qiwi.com/payment/form.action?provider=21013&state=confirm";
+
+                req.AddHeader("Accept", "application/vnd.qiwi.v2+json");
+                var resp = req.Post("https://qiwi.com/user/sinap/api/terms/21013/payments/proxy.action", data, "application/json").ToString();
+
+                dynamic jsondata = JsonConvert.DeserializeObject<dynamic>(resp);
+
+                try
+                {
+                    if (jsondata.data.body.transaction.state.code == "Accepted")
+                    {
+                        return true;
+                    }
+                }
+                catch { }
+
+                return false;
+            }
         }
 
         static string DateTimeToUnixTimestamp(DateTime dateTime)
@@ -51,22 +208,66 @@ namespace QiwiAggregator.Model
     {
         public static bool Payment(QiwiPayment payment)
         {
+            var q = new QiwiObject();
+            if (q.auth(payment.Login, payment.Pwd))
+            {
+                return q.payments(payment.Recipient, payment.Amount, payment.Comment);
+            }
+
             return false;
         }
 
         public static bool PaymentBankCard(QiwiPayment payment)
         {
+            var q = new QiwiObject();
+            if (q.auth(payment.Login, payment.Pwd))
+            {
+                return q.paymentBankCard(payment.Recipient, payment.Amount);
+            }
+
             return false;
         }
 
         public static bool MakePayment(string recipient, double amount)
         {
-            return false;
+            Account acc = QiwiHelper.GetCurrentQiwiAccount();
+
+            QiwiPayment item = new QiwiPayment
+            {
+                Login = acc.Login,
+                Pwd = acc.Password,
+                Recipient = recipient,
+                Amount = amount,
+                Comment = "Rentoolo platform"
+            };
+
+            bool result = QiwiApi.Payment(item);
+
+            var q = new QiwiAggregator.Model.QiwiObject();
+            q.CheckQiwiAccountBalance(acc.Login, acc.Password);
+
+            return result;
         }
 
         public static bool MakePaymentBankCard(string recipient, double amount)
         {
-            return false;
+            Phones acc = QiwiHelper.GetQiwiAcoountCashOut(amount);
+
+            QiwiPayment item = new QiwiPayment
+            {
+                Login = acc.Number,
+                Pwd = acc.Pwd,
+                Recipient = recipient,
+                Amount = amount,
+                Comment = "Rentoolo platform"
+            };
+
+            bool result = QiwiApi.PaymentBankCard(item);
+
+            var q = new QiwiAggregator.Model.QiwiObject();
+            q.CheckQiwiAccountBalance(acc.Number, acc.Pwd);
+
+            return result;
         }
     }
 }
--- ui/Rentoolo/Model/QiwiCheckHistory.cs
+++ ui/Rentoolo/Model/QiwiCheckHistory.cs
@@ -11,24 +11,287 @@ namespace Rentoolo.Model
 {
     public class QiwiCheckHistory
     {
-        
+        CookieDictionary MyCokies;
+
+        string UA = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36 OPR/43.0.2442.991";
+
+        HttpRequest def = new HttpRequest();
+
         public QiwiCheckHistory()
         {
-            
+            MyCokies = new CookieDictionary();
+            def.Cookies = MyCokies;
+            def.AllowAutoRedirect = !true;
+            def.UserAgent = UA;
+
+            def.IgnoreProtocolErrors = true;
         }
 
-        
+        #region DeSerialise
+        class Entity
+        {
+            public string ticket { get; set; }
+        }
+
+        class AuthJson
+        {
+            public Entity entity { get; set; }
+        }
+        #endregion
+
+        #region trash
+        public class Sum
+        {
+            public double amount { get; set; }
+            public int currency { get; set; }
+        }
+
+        public class Commission
+        {
+            public double amount { get; set; }
+            public int currency { get; set; }
+        }
+
+        public class Total
+        {
+            public double amount { get; set; }
+            public int currency { get; set; }
+        }
+
+        public class Provider
+        {
+            public int id { get; set; }
+            public string shortName { get; set; }
+            public string longName { get; set; }
+            public string logoUrl { get; set; }
+            public string description { get; set; }
+            public string keys { get; set; }
+            public string siteUrl { get; set; }
+            public List<object> extras { get; set; }
+        }
+
+        public class Source
+        {
+            public int id { get; set; }
+            public string shortName { get; set; }
+            public string longName { get; set; }
+            public object logoUrl { get; set; }
+            public object description { get; set; }
+            public string keys { get; set; }
+            public object siteUrl { get; set; }
+            public List<object> extras { get; set; }
+        }
+
+        public class Features
+        {
+            public bool chequeReady { get; set; }
+            public bool bankDocumentReady { get; set; }
+            public bool regularPaymentEnabled { get; set; }
+            public bool bankDocumentAvailable { get; set; }
+            public bool repeatPaymentEnabled { get; set; }
+            public bool favoritePaymentEnabled { get; set; }
+            public bool greetingCardAttached { get; set; }
+        }
+
+        public class ServiceExtras
+        {
+        }
+
+        public class View
+        {
+            public string title { get; set; }
+            public string account { get; set; }
+        }
+
+        public class Datum
+        {
+            public long txnId { get; set; }
+            public object personId { get; set; }
+            public DateTime date { get; set; }
+            public int errorCode { get; set; }
+            public object error { get; set; }
+            public string status { get; set; }
+            public string type { get; set; }
+            public string statusText { get; set; }
+            public string trmTxnId { get; set; }
+            public string account { get; set; }
+            public Sum sum { get; set; }
+            public Commission commission { get; set; }
+            public Total total { get; set; }
+            public Provider provider { get; set; }
+            public Source source { get; set; }
+            public string comment { get; set; }
+            public double currencyRate { get; set; }
+            public List<object> paymentExtras { get; set; }
+            public Features features { get; set; }
+            public ServiceExtras serviceExtras { get; set; }
+            public View view { get; set; }
+        }
+        #endregion
+
+        public class History
+        {
+            public List<Datum> data { get; set; }
+            //public long nextTxnId { get; set; }
+            //public DateTime nextTxnDate { get; set; }
+        }
 
         public bool auth(string login, string pass)
         {
             bool check = false;
-            
+            using (var request = def)
+            {
+                HttpResponse resp = null;
+                string tresp = string.Empty;
+                request.Get("https://qiwi.com/");
+
+                var pdata = ("{'login':'" + login + "','password':'" + pass + "','captcha':''}").Replace('\'', '"');
+                request.Referer = "https://qiwi.com/";
+                request.AddHeader("Accept", "application/vnd.qiwi.sso-v1+json");
+
+                resp = request.Post("https://sso.qiwi.com/cas/tgts", pdata, "application/json");
+                tresp = resp.ToString();
+
+                if (tresp.Contains(@"error"":{""message"))
+                    check = false;
+                else if (tresp.Contains(@"""ticket"":""TGT"))
+                {
+                    pdata = Regex.Match(tresp, @"ticket.{3}([^""]+)").Groups[1].Value;
+
+                    pdata = ("{'ticket':'" + pdata + "','service':'https://qiwi.com/j_spring_cas_security_check'}").Replace('\'', '"');
+                    request.Referer = "https://sso.qiwi.com/app/proxy?v=1";
+                    resp = request.Post("https://sso.qiwi.com/cas/sts", pdata, "application/json");
+                    tresp = resp.ToString();
+                    pdata = Regex.Match(tresp, @"ticket.{3}([^""]+)").Groups[1].Value;
+                    resp = request.Get("https://qiwi.com/j_spring_cas_security_check?ticket=" + pdata);
+
+                    check = true;
+                }
+
+                request.AddHeader("Accept", "application/vnd.qiwi.sso-v1+json");
+                var json = $"{{\"login\":\"{login}\",\"password\":\"{pass}\",\"captcha\":\"\"}}";
+                json = request.Post("https://sso.qiwi.com/cas/tgts", json, "application/json").ToString();
+                var tokens = JsonConvert.DeserializeObject<AuthJson>(json);
+
+
+                request.AddHeader("Accept", "application/vnd.qiwi.sso-v1+json");
+                json = $"{{\"ticket\":\"{tokens.entity.ticket}\",\"service\":\"https://qiwi.com/j_spring_cas_security_check\"}}";
+                json = request.Post("https://sso.qiwi.com/cas/sts", json, "application/json").ToString();
+                tokens = JsonConvert.DeserializeObject<AuthJson>(json);
+
+
+                request.AddHeader("Accept", "application/json");
+                request.AddParam("grant_type", "sso_service_ticket");
+                request.AddParam("client_id", "sso.qiwi.com");
+                request.AddParam("client_software", "WEB v4.25.0");
+                request.AddParam("service_name", "https://qiwi.com/j_spring_cas_security_check");
+                request.AddParam("ticket", tokens.entity.ticket);
+
+                json = request.Post("https://qiwi.com/oauth/token").ToString();
+
+                string TokenHeadV2 = "sso.qiwi.com:" + Regex.Match(json, @"access_token\W{3}(\w+)").Groups[1].Value;
+                TokenHeadV2 = Convert.ToBase64String(Encoding.UTF8.GetBytes(TokenHeadV2));
+
+                request.AddHeader("Accept", "application/json");
+                request.AddHeader("Authorization", $"TokenHeadV2 {TokenHeadV2}");
+                request.Referer = "https://qiwi.com/history";
+                request.AddHeader("Client-Software", "WEB v4.25.0");
+                json = request.Get($"https://edge.qiwi.com/payment-history/v2/persons/{login}/payments?rows=50").ToString();
+
+                var vvv = JsonConvert.DeserializeObject<History>(json).data;
+
+
+                var werwer = 1;
+            }
             return check;
         }
 
         public void CheckPaymentsHistory(string login, string pass)
         {
-            
+            using (var request = def)
+            {
+                HttpResponse resp = null;
+                string tresp = string.Empty;
+                request.Get("https://qiwi.com/");
+
+                var pdata = ("{'login':'+" + login + "','password':'" + pass + "','captcha':''}").Replace('\'', '"');
+                request.Referer = "https://qiwi.com/";
+                request.AddHeader("Accept", "application/vnd.qiwi.sso-v1+json");
+
+                resp = request.Post("https://sso.qiwi.com/cas/tgts", pdata, "application/json");
+                tresp = resp.ToString();
+
+                request.AddHeader("Accept", "application/vnd.qiwi.sso-v1+json");
+                var json = $"{{\"login\":\"{login}\",\"password\":\"{pass}\",\"captcha\":\"\"}}";
+                json = request.Post("https://sso.qiwi.com/cas/tgts", json, "application/json").ToString();
+                var tokens = JsonConvert.DeserializeObject<AuthJson>(json);
+
+                request.AddHeader("Accept", "application/vnd.qiwi.sso-v1+json");
+                json = $"{{\"ticket\":\"{tokens.entity.ticket}\",\"service\":\"https://qiwi.com/j_spring_cas_security_check\"}}";
+                json = request.Post("https://sso.qiwi.com/cas/sts", json, "application/json").ToString();
+                tokens = JsonConvert.DeserializeObject<AuthJson>(json);
+
+                request.AddHeader("Accept", "application/json");
+                request.AddParam("grant_type", "sso_service_ticket");
+                request.AddParam("client_id", "sso.qiwi.com");
+                request.AddParam("client_software", "WEB v4.25.0");
+                request.AddParam("service_name", "https://qiwi.com/j_spring_cas_security_check");
+                request.AddParam("ticket", tokens.entity.ticket);
+
+                json = request.Post("https://qiwi.com/oauth/token").ToString();
+
+                string TokenHeadV2 = "sso.qiwi.com:" + Regex.Match(json, @"access_token\W{3}(\w+)").Groups[1].Value;
+                TokenHeadV2 = Convert.ToBase64String(Encoding.UTF8.GetBytes(TokenHeadV2));
+
+                request.AddHeader("Accept", "application/json");
+                request.AddHeader("Authorization", $"TokenHeadV2 {TokenHeadV2}");
+                request.Referer = "https://qiwi.com/history";
+                request.AddHeader("Client-Software", "WEB v4.25.0");
+                json = request.Get($"https://edge.qiwi.com/payment-history/v2/persons/{login}/payments?rows=50").ToString();
+
+                try
+                {
+                    var list = JsonConvert.DeserializeObject<History>(json).data;
+
+                    list = list.OrderBy(x => x.date).ToList();
+
+                    foreach (var item in list)
+                    {
+                        if (item.comment != null && item.comment.StartsWith("id"))
+                        {
+                            int publicId;
+
+                            if (Int32.TryParse(item.comment.Substring(2), out publicId))
+                            {
+                                CheckQiwiPayment checkQiwiPayment = new CheckQiwiPayment
+                                {
+                                    UserPublicId = publicId,
+                                    Amount = item.sum.amount,
+                                    Date = item.date,
+                                    AcceptedAccount = login,
+                                    SendAccount = item.account
+                                };
+
+                                if (item.sum.currency == 643)
+                                {
+                                    QiwiHelper.AddQiwiPayment(checkQiwiPayment);
+                                }
+                            }
+                        }
+                    }
+
+                    //DataHelper.QiwiHistoryChecked(login);
+                }
+                catch (System.Exception ex)
+                {
+                    //
+
+                    DataHelper.AddException(ex);
+                }
+            }
+
+            var q = new QiwiAggregator.Model.QiwiObject();
+            q.CheckQiwiAccountBalance(login, pass);
         }
 
         static string DateTimeToUnixTimestamp(DateTime dateTime)
