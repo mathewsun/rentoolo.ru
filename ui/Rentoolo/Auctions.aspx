ef9bb2829144e727edc5b29188eb66faa775bd36
Antares488 <antares5988@gmail.com>
2020-05-11 19:27:35 +0300

    add Auctions page

---
+++ ui/Rentoolo/Auctions.aspx
@@ -0,0 +1,6 @@
+﻿<%@ Page Title="Аукционы" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Auctions.aspx.cs" Inherits="Rentoolo.Auctions" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="HeadContent" runat="server">
+
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="MainContent" runat="server">
+</asp:Content>
---
+++ ui/Rentoolo/Auctions.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Rentoolo.Model;
+
+namespace Rentoolo
+{
+    public partial class Auctions : System.Web.UI.Page
+    {
+        public List<Model.Auctions> ListItems;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                ListItems = AuctionsHelper.GetAllAuctions();
+            }
+        }
+    }
+}
