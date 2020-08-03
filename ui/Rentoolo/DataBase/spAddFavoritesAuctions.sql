151b3b7a2d4352cb63bbcd2e6cbda1b7f2d645c6
a.rosokhatyy <a.rosokhatyy@click2.money>
2020-08-03 23:26:58 +0300

    add spAddFavoritesAuctions

---
+++ ui/Rentoolo/DataBase/spAddFavoritesAuctions.sql
@@ -0,0 +1,17 @@
+﻿CREATE PROCEDURE  [dbo].[spAddFavoritesAuctions]
+@UserId uniqueidentifier, @AuctionId bigint
+AS
+BEGIN
+   IF NOT EXISTS (SELECT * FROM FavoritesAuctions 
+                   WHERE UserId = @UserId
+                   AND AuctionId = @AuctionId)
+   BEGIN
+       INSERT INTO FavoritesAuctions (UserId, AuctionId)
+       VALUES (@UserId, @AuctionId)
+   END
+END
+GO
+
+GRANT EXECUTE
+    ON OBJECT::[dbo].[spAddFavorites] TO PUBLIC
+    AS [dbo];
