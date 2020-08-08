b2bcbe5d09e185f1ab0eb98c18ffc6fd3ac4ee1d
matthew <mathew.dubrovin@gmail.com>
2020-08-08 14:27:05 +0300

    add kitchen controller

---
+++ ui/Rentoolo/Controllers/KitchenController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Rentoolo.Controllers.KitchenModel;
+
+namespace Rentoolo.Controllers
+{
+    public class KitchenController : ApiController
+    {
+        public IHttpActionResult GetTop5Recipes()
+        {
+            List<Recipes> result = new List<Recipes>();
+
+            result.Add(new Recipes
+            {
+                Name = "Воздушные блины на пахте",
+                ImgUrl = "/img/kitchen/vozdushnie-blini-na-pahte.jpg",
+                CountLikes = 720,
+                TimeMinutesToCook = 15
+            });
+
+            return Ok(result);
+        }
+    }
+}
---
+++ ui/Rentoolo/Controllers/KitchenModel/Recipes.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Rentoolo.Controllers.KitchenModel
+{
+    public class Recipes
+    {
+        public string Name { get; set; }
+
+        public string ImgUrl { get; set; }
+
+        public int TimeMinutesToCook { get; set; }
+
+        public int CountLikes { get; set; }
+    }
+}
