a0c95dec4868c4551a9364d028b087a98cd06404
a.rosokhatyy <a.rosokhatyy@click2.money>
2020-08-11 01:00:58 +0300

    add recipe table and event (getrecipe)

---
+++ ui/Rentoolo/Controllers/RecipeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Rentoolo.Model;
+
+
+namespace Rentoolo.Controllers
+{
+    public class RecipeController : ApiController
+    {
+        // GET api/<controller>
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<controller>/5
+        public IHttpActionResult Get(int id)
+        {
+            Recipes recipe = KitchenDataHelper.GetRecipe(id);
+
+            return Json(recipe);
+        }
+
+        // POST api/<controller>
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<controller>/5
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
