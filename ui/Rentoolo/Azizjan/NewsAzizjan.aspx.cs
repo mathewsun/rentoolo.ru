f7587a67c02fece02dee48dfcb69c9a76f851637
Azizjan Ayupov <litpulla@mail.ru>
2020-11-12 16:52:03 +0600

    Add test page NewsAzizjan

---
+++ ui/Rentoolo/Azizjan/NewsAzizjan.aspx.cs
@@ -0,0 +1,17 @@
+﻿using Rentoolo.Model;
+using System;
+using System.Collections.Generic;
+using System.Web.UI;
+
+namespace Rentoolo.Azizjan
+{
+    public partial class NewsAzizjan : Page
+    {
+        public List<Model.NewsAzizjan> ListNews;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            ListNews = DataHelperAzizjan.GetActiveNews();
+        }
+    }
+}
