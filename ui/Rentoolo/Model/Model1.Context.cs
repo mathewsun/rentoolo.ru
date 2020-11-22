c6ee7f55d1c41ad6069db239ec98c228fa1878c3
towardsbackwards <Protuberance23>
2020-11-22 11:27:50 +0300

    add test page NewsTowardsbackwards

--- ui/Rentoolo/Model/Model1.Context.cs
+++ ui/Rentoolo/Model/Model1.Context.cs
@@ -62,6 +62,7 @@ namespace Rentoolo.Model
         public virtual DbSet<Memberships> Memberships { get; set; }
         public virtual DbSet<NewAezakmi> NewAezakmi { get; set; }
         public virtual DbSet<News> News { get; set; }
+        public virtual DbSet<News_towardsbackwards> News_towardsbackwards { get; set; }
         public virtual DbSet<NewsAzizjan> NewsAzizjan { get; set; }
         public virtual DbSet<NewsEducation> NewsEducation { get; set; }
         public virtual DbSet<NewsGGdotNET> NewsGGdotNET { get; set; }
