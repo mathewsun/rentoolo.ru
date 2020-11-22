5e689e3d30b28987b722e68690474db0e4739d07
towardsbackwards <Protuberance23>
2020-11-22 11:34:39 +0300

    add test page NewsTowardsbackwards

---
+++ ui/Rentoolo/Model/DataHelperTowardsbackwards.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Rentoolo.Model
+{
+    public static class DataHelperTowardsbackwards
+    {
+        public static List<News_towardsbackwards> GetActiveNews()
+        {
+            using (var ctx = new RentooloEntities())
+            {
+                var list = ctx.News_towardsbackwards.Where(x => x.Active).OrderByDescending(x => x.Date).ToList();
+
+                return list;
+            }
+        }
+    }
+}
