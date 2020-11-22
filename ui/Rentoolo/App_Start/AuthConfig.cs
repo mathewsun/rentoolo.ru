bff5245c817560998d533d690460b4d8c92bcf8d
qwertyuiopasdfghjklzxcvbnm <qweqw.qwe.88@mail.ru>
2020-11-22 14:16:51 +0300

    added facebook auth

--- ui/Rentoolo/App_Start/AuthConfig.cs
+++ ui/Rentoolo/App_Start/AuthConfig.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using Microsoft.AspNet.Membership.OpenAuth;
 using DotNetOpenAuth.GoogleOAuth2;
+using DotNetOpenAuth.FacebookOAuth2;
 
 namespace Rentoolo
 {
@@ -11,6 +12,7 @@ namespace Rentoolo
     {
         public static void RegisterOpenAuth()
         {
+            var extraData = new Dictionary<string, string>();
             // See http://go.microsoft.com/fwlink/?LinkId=252803 for details on setting up this ASP.NET
             // application to support logging in via external services.
 
@@ -18,19 +20,19 @@ namespace Rentoolo
             //    consumerKey: "your Twitter consumer key",
             //    consumerSecret: "your Twitter consumer secret");
 
-            //OpenAuth.AuthenticationClients.AddFacebook(
-            //    appId: "your Facebook app id",
-            //    appSecret: "your Facebook app secret");
+            var clientF = new FacebookOAuth2Client("748096399254392",
+                "38c86be9252aba1dde6307ba114c4a79");
+            OpenAuth.AuthenticationClients.Add("Facebook", () => clientF, extraData);
 
             //OpenAuth.AuthenticationClients.AddMicrosoft(
             //    clientId: "your Microsoft account client id",
             //    clientSecret: "your Microsoft account client secret");
 
             //OpenAuth.AuthenticationClients.AddGoogle();
-            var client = new GoogleOAuth2Client("337461708184-a40ahhpbvn24h2ub6gbqn6ljn2h1ath9.apps.googleusercontent.com",
+            var clientG = new GoogleOAuth2Client("337461708184-a40ahhpbvn24h2ub6gbqn6ljn2h1ath9.apps.googleusercontent.com",
                 "iDDgWvaWH6RdOMUzdWAyfLpP");
-            var extraData = new Dictionary<string, string>();
-            OpenAuth.AuthenticationClients.Add("Google", () => client, extraData);
+
+            OpenAuth.AuthenticationClients.Add("Google", () => clientG, extraData);
         }
     }
 }
