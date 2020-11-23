5645f85972f0446de0ce6ac6b73ab888c748a881
Necromant24 <kesha.tkachenko2017@mail.ru>
2020-11-23 23:06:44 +0300

    move procedures to folder

--- ui/Rentoolo/DataBase/storedProcedures/spGetExchangeProducts.sql
+++ ui/Rentoolo/DataBase/storedProcedures/spGetExchangeProducts.sql
@@ -1,38 +1,41 @@
 ﻿USE [Rentoolo]
 GO
-/****** Object:  StoredProcedure [dbo].[spGetExchangeProducts]    Script Date: 22.11.2020 22:46:14 ******/
+/****** Object:  StoredProcedure [dbo].[spGetExchangeProducts]    Script Date: 23.11.2020 22:42:29 ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
 ALTER PROCEDURE  [dbo].[spGetExchangeProducts]
-(@query nvarchar)
+(@query nvarchar(30))
 AS
 BEGIN
 
 	SELECT 
 		expr.[Id]
-      ,[AdvertId]
-      ,[Comment]
-      ,[WantedObject]
-      ,[Header]
-	  ,[Category]
-      ,[Name]
-      ,[Created]
-      ,[Price]
-      ,[Address]
-      ,[IsApproved]
-      ,[Subcategory]
-      ,[Color]
-      ,[Vin]
-      ,[Brand]
+      ,expr.[AdvertId]
+      ,expr.[Comment]
+      ,expr.[WantedObject]
+      ,expr.[Header]
+	  ,adv.[Category]
+      ,adv.[Name]
+      ,adv.[Created]
+      ,adv.[Price]
+      ,adv.[Address]
+      ,adv.[IsApproved]
+      ,adv.[Subcategory]
+      ,adv.[Color]
+      ,adv.[Vin]
+      ,adv.[Brand]
 
-
-	FROM dbo.ExchangeProducts as expr
-	LEFT JOIN dbo.Adverts as adv
+	  
+	FROM dbo.ExchangeProducts AS expr
+	LEFT JOIN dbo.Adverts AS adv
 	ON expr.AdvertId = adv.Id 
 	WHERE (adv.Name LIKE @query) OR (adv.Description LIKE @query) 
 	OR (expr.Header LIKE @query) OR (expr.Comment LIKE @query) OR (expr.WantedObject LIKE @query)
 
 END
 
+GRANT EXECUTE
+    ON OBJECT::[dbo].[spGetExchangeProducts] TO PUBLIC
+    AS [dbo];
