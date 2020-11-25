06c2860484c953adb56c327be2288664e8750111
AlexPigalyov <alex.pigalyov@gmail.com>
2020-11-25 03:14:35 +0300

    add test page NewsAlexPigalyov

---
+++ ui/Rentoolo/Model/DataHelperAlexPigalyov.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Rentoolo.Model
+{
+    public static class DataHelperAlexPigalyov
+    {
+        public static List<NewsAlexPigalyov> GetActiveNews()
+        {
+            using (var ctx = new RentooloEntities())
+            {
+                var list = ctx.NewsAlexPigalyov.Where(x => x.Active.Value).OrderByDescending(x => x.Date).ToList();
+
+                return list;
+            }
+        }
+    }
+}
