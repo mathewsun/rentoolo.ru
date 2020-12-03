9a04d39174133199a63fa1eb0387cf8aa8154d52
vladislav0282 <ubpsv31460@yandex.ru>
2020-12-03 11:25:04 +0300

    add test page NewsVlad

---
+++ ui/Rentoolo/Model/DataHelperVlad.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Rentoolo.Model
+{
+    public static class DataHelperVlad
+    {
+        public static List<NewsVlad> GetActiveNews()
+        {
+            using (var ctx = new RentooloEntities())
+            {
+                var list = ctx.NewsVlad.Where(x => x.Active).OrderByDescending(x => x.Date).ToList();
+
+                return list;
+            }
+        }
+    }
+}
